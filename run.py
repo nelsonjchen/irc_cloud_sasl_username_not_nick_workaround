b181e57fddde4b29be4c78decd334f8b24702918
Nelson Chen <crazysim@gmail.com>
2019-03-16 19:40:19 -0700

    It works. But gotta dogfood it first.

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 import asyncio
 import json
 import os
+import sys
+
 import requests
 import websockets
 
 IRCCLOUD_EMAIL = os.environ['IRCCLOUD_EMAIL']
 IRCCLOUD_PASSWORD = os.environ['IRCCLOUD_PASSWORD']
 # Fancy Server Name in IRCCloud GUI, eg. Rizon, SynIRC, freenode, Secret Server
-IRC_SERVER_IRCCLOUD_NAME = os.environ['IRC_SERVER_IRCCLOUD_NAME']
+IRC_SERVER_IRCCLOUD_NETWORK_NAME = os.environ['IRC_SERVER_IRCCLOUD_NETWORK_NAME']
 # user(12333)
 IRC_SERVER_IRCCLOUD_SASL_NAME = os.environ['IRC_SERVER_IRCCLOUD_SASL_NAME']
 # xXxDongSmasherVegeta420xXx
@@ -49,23 +51,54 @@ async def consumer(message_json: str, websocket: websockets.client.WebSocketClie
             for message_json in backlog:
                 await consumer(json.dumps(message_json), websocket)
         if message['type'] == 'makeserver':
-            if message['name'] != IRC_SERVER_IRCCLOUD_NAME:
+            if message['name'] != IRC_SERVER_IRCCLOUD_NETWORK_NAME:
                 return
-            if not message['disconnected']:
+            print("--- CONSIDERING NETWORK ---")
+            edit_server_values = {
+                "_method": 'edit-server',
+                "cid": message['cid'],
+                "hostname": message['hostname'],
+                "port": message['port'],
+                "ssl": message['ssl'],
+                "netname": message['name'],
+                "nickname": message['nick'],
+                "realname": message['realname'],
+                "server_pass": message['server_pass'],
+                "nspass": message['nickserv_pass'],
+                "joincommands": message['join_commands'],
+            }
+            irccloud_network_id = message['cid']
+
+            if message['status'] == 'connected' and message['nick'] != IRC_SERVER_IRCCLOUD_NICK_NAME:
+                print("--- ALREADY CONNECTED BUT BAD NICK ---")
+                edit_server_values['nickname'] = IRC_SERVER_IRCCLOUD_NICK_NAME
+                await websocket.send(json.dumps(edit_server_values))
                 return
-            # Begin Reconnect Process
+
+            if message['status'] != 'connected' and message['nick'] != IRC_SERVER_IRCCLOUD_SASL_NAME:
+                # Set the name to be the correct nickname
+                edit_server_values['nickname'] = IRC_SERVER_IRCCLOUD_SASL_NAME
+                await websocket.send(json.dumps(edit_server_values))
+                # Force a Disconnect
+                # Force a Reconnect
+                # sys.exit(0)
+                pass
+
+            if message['status'] == 'connected' and message['nick'] == IRC_SERVER_IRCCLOUD_NICK_NAME:
+                print("--- ALL IS WELL ---")
+                sys.exit(0)
 
 
-async def hello():
+async def connect_and_check():
     # noinspection PyUnusedLocal
     websocket: websockets.client.WebSocketClientProtocol
     async with websockets.connect(
-        'wss://api.irccloud.com/',
-        ssl=True,
-        extra_headers={
-            'Cookie': f'session={session}',
-            'Origin': 'https://api.irccloud.com',
-        }
+            'wss://api.irccloud.com/',
+            ssl=True,
+            extra_headers={
+                'Cookie': f'session={session}',
+                'Origin': 'https://api.irccloud.com',
+            }
     ) as websocket:
         # noinspection PyUnusedLocal
         message_json: str
@@ -73,4 +106,20 @@ async def hello():
             await consumer(message_json, websocket)
 
 
-asyncio.get_event_loop().run_until_complete(hello())
+async def check_multiple_times():
+    for _i in range(5):
+        try:
+            await asyncio.wait_for(connect_and_check(), timeout=5)
+        except asyncio.TimeoutError:
+            print(f'#### Check {_i + 1}! ####')
+
+
+async def main():
+    # Wait for at most 100 seconds
+    try:
+        await asyncio.wait_for(check_multiple_times(), timeout=30)
+    except asyncio.TimeoutError:
+        print('Finished!')
+
+
+asyncio.get_event_loop().run_until_complete(main())
